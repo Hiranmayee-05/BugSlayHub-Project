3a479d5779f79203a66d94cafa63114ce1bfd63d
Satyapriya mishra <106869525+mishrasatyapriya9@users.noreply.github.com>
2023-07-29 12:20:36 +0530

    Update AuthContext.jsx

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+//this is the making authentication her which is used in the diff code files like prodeucts page
 import { createContext } from "react";
 export const AuthContext = createContext();
 const AuthContextProvider = ({ children }) => {
